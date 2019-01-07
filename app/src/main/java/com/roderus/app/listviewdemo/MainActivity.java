bf6432794328538aee98d20a811396d9501abefe
Prof. Dr. Helmut Roderus <helmut.roderus@hs-ansbach.de>
2019-01-07 20:44:38 +0100

    3rd commit: Eventhandling für OnItemClick: Zeigt Toast an

--- app/src/main/java/com/roderus/app/listviewdemo/MainActivity.java
+++ app/src/main/java/com/roderus/app/listviewdemo/MainActivity.java
@@ -1,12 +1,19 @@
 package com.roderus.app.listviewdemo;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+import static android.widget.Toast.LENGTH_LONG;
+
+public class MainActivity extends Activity {
 
     private ArrayList<String> namensliste = new ArrayList<>();
 
@@ -21,6 +28,15 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, namensliste );
         lv.setAdapter(adapter);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                            @Override
+                                            public void onItemClick(AdapterView<?> parent, final View view, int position, long id) {
+                                                final String clickedName = (String) parent.getItemAtPosition(position);
+                                                Toast.makeText(getApplicationContext(), id+": "+clickedName, Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+        );
         
     }
 
